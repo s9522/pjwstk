36f48dfaec60b313579fa86f463e1f7b7b35470f
smiodek <smiodek@172.23.129.49>
2015-01-16 01:18:08 +0100

    porządki

---
+++ Pjatk.Pab.Books.BLL/Facades/IAuthors.cs
@@ -0,0 +1,18 @@
+﻿using Pjatk.Pab.Books.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pjatk.Pab.Books.BLL.Facades
+{
+    public interface IAuthors
+    {
+        void CreateAuthor(Author author);
+        void UpdateAuthor(Author author);
+        void RemoveAuthor(Author author);
+        IEnumerable<Author> GetAllAuthors();
+        Author GetAuthorById(int id);
+    }
+}
--- Pjatk.Pab.Books.BLL/Facades/IBooks.cs
+++ Pjatk.Pab.Books.BLL/Facades/IBooks.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pjatk.Pab.Books.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Pjatk.Pab.Books.BLL.Facades
 {
-    public class IBooks
-    {
+    public interface IBooks
+    { 
+        void CreateBook(Book book);
+        void UpdateBook(Book book);
+        void RemoveBook(Book book);
+        IEnumerable<Book> GetAllBooks();
+        Book GetBookById(int id);
     }
 }
