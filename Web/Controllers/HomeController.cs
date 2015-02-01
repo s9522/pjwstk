08e7225b9cb2ae6c361e4a0ffe36fc638cee92af
smiodek <smiodek@172.23.129.49>
2015-02-01 01:57:31 +0100

    autentykacja

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Pjatk.Pab.Books.BLL.Facades;
+using Web.Models;
+
 namespace Web.Controllers
 {
     public class HomeController : Controller
@@ -22,5 +25,50 @@ namespace Web.Controllers
         {
             return RedirectToAction("Index", "Authors");
         }
+
+        //
+        // GET: /Account/LogOn
+        [AllowAnonymous]
+        public ActionResult LogOn()
+        {
+            LogOnViewModel model = new LogOnViewModel();
+
+            return View(model);
+        }
+
+        //
+        // POST: /Account/LogOn
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult LogOn(LogOnViewModel model, string returnUrl)
+        {
+            if (this.ModelState.IsValid && FormsAuthentication.Authenticate(model.UserName, model.Password))
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                if (this.Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+           
+            this.ModelState.AddModelError("", "Zła nazwa użytkownika lub hasło");
+            return View(model);
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
     }
 }
---
+++ Web/Models/LogOnViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class LogOnViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj")]
+        public bool RememberMe { get; set; }
+    }
+}
